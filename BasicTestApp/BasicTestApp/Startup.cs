c061f05a1aa6a3c95d3f9cc04e673c7b0730277f
Tiago Fenix Soares de Barros <tiagofenixbarros@gmail.com>
2017-11-29 17:02:25 -0200

    Basic Test App creation

---
+++ BasicTestApp/BasicTestApp/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BasicTestApp
+{
+    public class Startup
+    {
+        public const string LOCAL_ENVIROMENT = "Local";
+
+        public ILogger<Startup> _logger;
+
+        public Startup(IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            _logger = logger;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env,
+                              IConfiguration configuration)
+        {
+            if (env.IsDevelopment())
+            {
+                //app.UseDeveloperExceptionPage();
+            }
+            
+
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(configuration["DeveloperName"]);
+            });
+        }
+    }
+}
